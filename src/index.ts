aaae32ec67de08e906c0ac9fc52a880a8d17859c
sugar <engineerzjl@foxmail.com>
2021-09-27 13:38:24 +0800

    chore: format stule

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,48 @@
-import type { Plugin } from 'vite'
-import { createLabelKey, timeEnd, timeStart } from './utils'
+import type { Plugin } from 'vite';
+import { createLabelKey, timeEnd, timeStart } from './utils';
 
-import type { PluginOptions } from './types'
-
+import type { PluginOptions } from './types';
 
 export default function Monitor(ops: PluginOptions = {}): Plugin {
-    const { log } = ops
-    
-    if (log) {
-        global[Symbol.for('_monitorLog')] = log
-    }
+  const { log } = ops;
 
-    global[createLabelKey('ready')] = global.__vite_start_time
-    
-    return {
-        name: 'vite-plugin-monitor',
-        apply: 'serve',
-        // hack获取启动耗时|HMR耗时
-        configureServer(server) {
-            const info = server.config.logger.info
-            server.config.logger.info = function _info(str) {
-                info.apply(this, arguments)
-                if (str.includes('ready in')) {
-                    timeEnd('ready')
-                }
-                if (str.indexOf('hmr update') >= 0) {
-                    timeStart('hmr_update')
-                }
-            }
-            server.middlewares.use(async (req, res, next) => {
-                const { search } = new URL(req.url, `http://${req.headers.host}`);
-                  if (
-                    search.indexOf('import&') >= 0
-                  ) {
-                    const end = res.end
-                    res.end = function _end() {
-                      end.apply(this, arguments)
-                      timeEnd('hmr_update')
-                    }
-                  }
-                next()
-            })
-        },
-    }
+  if (log) {
+    global[Symbol.for('_monitorLog')] = log;
+  }
+
+  global[createLabelKey('ready')] = global.__vite_start_time;
+
+  return {
+    name: 'vite-plugin-monitor',
+    apply: 'serve',
+    // hack获取启动耗时|HMR耗时
+    configureServer(server) {
+      const { info } = server.config.logger;
+      // eslint-disable-next-line no-param-reassign
+      server.config.logger.info = function _info(str) {
+        // eslint-disable-next-line prefer-rest-params
+        info.apply(this, arguments);
+        if (str.includes('ready in')) {
+          timeEnd('ready');
+        }
+        if (str.indexOf('hmr update') >= 0) {
+          timeStart('hmr_update');
+        }
+      };
+      server.middlewares.use(async (req, res, next) => {
+        const { search } = new URL(req.url, `http://${req.headers.host}`);
+        if (
+          search.indexOf('import&') >= 0
+        ) {
+          const { end } = res;
+          res.end = function _end() {
+            // eslint-disable-next-line prefer-rest-params
+            end.apply(this, arguments);
+            timeEnd('hmr_update');
+          };
+        }
+        next();
+      });
+    },
+  };
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,41 +1,41 @@
-import chalk from 'chalk'
+import chalk from 'chalk';
+
+const pluginName = 'vite-plugin-monitor';
+export function createLabelKey(label:string) {
+  return `_${pluginName}_${label}`;
+}
+
+export function getOriginLabel(labelKey:string) {
+  return labelKey.replace(`_${pluginName}_`, '');
+}
 
 export function timeStart(label: string) {
-    const k = createLabelKey(label)
-    if (global[k]) {
-        return
-    }
-    global[k] = Date.now()
+  const k = createLabelKey(label);
+  if (global[k]) {
+    return;
+  }
+  global[k] = Date.now();
 }
 
 export function timeEnd(label: string) {
-    const k = createLabelKey(label)
-    
-    if (!global[k]) {
-        return {
-            label,
-            time:-1
-        }
-    }
-    const time = Date.now() - global[k]
-    // 清空
-    global[k] = null
+  const k = createLabelKey(label);
 
-    if (global[Symbol.for('_monitorLog')]) {
-        console.log(chalk.yellow(label), chalk.blue(`${time}ms`))
-    }
-
+  if (!global[k]) {
     return {
-        label,
-        time
-    }
-}
+      label,
+      time: -1,
+    };
+  }
+  const time = Date.now() - global[k];
+  // 清空
+  global[k] = null;
 
-const  pluginName = 'vite-plugin-monitor'
-export function createLabelKey(label:string){
-    return `_${pluginName}_${label}`
-}
+  if (global[Symbol.for('_monitorLog')]) {
+    console.log(chalk.yellow(label), chalk.blue(`${time}ms`));
+  }
 
-export function getOriginLabel(labelKey:string){
-    return labelKey.replace(`_${pluginName}_`,'')
+  return {
+    label,
+    time,
+  };
 }
